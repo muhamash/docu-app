e6bccce7a967babb55cc8fb8f986deb3e4261083
muhamash <muhammad-ashraful@outlook.com>
2024-09-16 22:57:32 +0600

    working on

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,5 +1,7 @@
-import Link from "next/link";
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
+import Link from "next/link";
 
 const Landing = () => {
   return (
